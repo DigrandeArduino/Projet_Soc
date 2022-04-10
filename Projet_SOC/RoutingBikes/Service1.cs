a207a15957a79f6c0002e18564a06fbd4e06a306
Thomas Di Grande <thomas.di-grande@etu.univ-cotedazur.fr>
2022-04-10 19:04:01 +0200

    NewCache

---
+++ Projet_SOC/RoutingBikes/Service1.cs
@@ -0,0 +1,33 @@
+﻿using ProxyCacheServer;
+using RoutingBikes.Proxy;
+using System;
+using System.Threading.Tasks;
+
+namespace RoutingBikes
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public async Task<string> GetData(int value)
+        {
+            Service1Client client = new Service1Client();
+            JCDecauxItem stations = client.GetAllStation();
+            client.Close();
+
+            return string.Format("You entered");
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
--- Projet_SOC/WebProxyService/Program.cs
+++ Projet_SOC/WebProxyService/Program.cs
@@ -135,6 +135,8 @@ namespace WebProxyService
         }
 
     }
+
+
 }
 
 internal class InternalCallAPI
